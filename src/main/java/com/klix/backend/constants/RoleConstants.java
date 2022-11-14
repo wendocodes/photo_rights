b80f48eb477f06c99dabf79d7c9df562f6b1e185
Joyce Wendo <wendojoyce.de@gmail.com>
2022-11-14 23:00:28 +0100

    Add files via upload

---
+++ src/main/java/com/klix/backend/constants/RoleConstants.java
@@ -0,0 +1,7 @@
+package com.klix.backend.constants;
+
+public final class RoleConstants {
+    public static final String user = "USER";
+    public static final String admin = "ADMINISTRATOR";
+    public static final String developer = "DEVELOPER";
+}
